9167fdf01ff9943fbfdfa7be688d8cc8868143f6
Mihaylov-Ivan <im566@bath.ac.uk>
2023-05-13 16:00:22 +0100

    Fix script 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    let answer = "";
+    
+    for (let i=0; i<n; i++)
+    {
+        answer += word;
+    }
+    
+    if (n < 0)
+        return "ERROR";
+    else
+        return answer;
 };
 
 // Do not edit below this line
