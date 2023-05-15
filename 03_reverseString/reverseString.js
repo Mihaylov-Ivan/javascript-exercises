36255b5262cb511137a545f9ecf662fd2c24a99e
Mihaylov-Ivan <im566@bath.ac.uk>
2023-05-15 15:30:10 +0100

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let characters = [],
+        newWord = "";
 
+    characters = word.split("");
+    let counter = characters.length - 1;
+
+    characters.forEach(char => {
+        newWord += `${characters[counter]}`;
+        counter--;
+    });
+
+    return newWord;
 };
 
 // Do not edit below this line
