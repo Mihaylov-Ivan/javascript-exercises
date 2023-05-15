6fd3f15d8c78aad31daeffb07081e8dd3aadd4fe
Mihaylov-Ivan <im566@bath.ac.uk>
2023-05-15 19:05:19 +0100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+// const removeFromArray = 
+function removeFromArray(array, ...args) {
+    let index = 0;
 
+    args.forEach(arg => {
+        if (array.includes(arg)){
+            index = array.indexOf(arg);
+            array.splice(index, 1);
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
