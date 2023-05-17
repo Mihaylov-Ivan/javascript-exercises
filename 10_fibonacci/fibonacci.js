77afc1a86806af1ba901acda4d629ddca47e3a04
Mihaylov-Ivan <im566@bath.ac.uk>
2023-05-17 23:33:02 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
 
+    if (n<0)
+        return "OOPS";
+    else if (n==0)
+        return 0;
+
+    let arr = [0, 1];
+    
+    for (let i = 1; i<=n; i++)
+        arr[i+1] = arr[i] + arr[i-1];
+    return arr[n];
 };
 
 // Do not edit below this line
