392b4c98e3073ddd04dba0c1d25c64b2389e1c3c
ivamih2k <mihaylov.ivan@siemens.com>
2023-05-27 19:17:09 +0100

    Random work

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -25,10 +25,12 @@ const reverseString = require("../03_reverseString/reverseString");
 
 const palindromes = function (string) {
     // This regex matches anything that is not the letter a-z in the
-    // whole string and replaces it with ""
+    // whole string and replaces it with "".
+    // It returns true if the string is a palindrome by removing spaces, reversing it, joining the letters again
+    // and comparing to original
     const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
     return processedString.split("").reverse().join("") == processedString;
-  };
+};
   
 
 console.log(palindromes("A car, a man, a maraca."))
